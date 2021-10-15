94690bfa8200a4a67f12e86aba8002514fb39233
feherstefan <stefanflorinf@gmail.com>
2021-10-15 09:12:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IntroProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("X=");
+            string xAsString = Console.ReadLine();
+            x = int.Parse(xAsString);
+
+            int y;            
+            Console.Write("Y=");
+            string yAsString = Console.ReadLine();
+            y = int.Parse(yAsString);
+
+            bool isOverflow = ((x + y) < int.MaxValue);
+            if (!isOverflow)
+            {
+                int sum = checked(x + y);
+                Console.WriteLine("Sum = " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Arithmetic overflow");
+            }
+
+        }
+    }
+}
