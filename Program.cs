dd56fa8e2e3089e3079f5f58dc589b133dc91cfb
feherstefan <stefanflorinf@gmail.com>
2021-10-17 23:28:07 +0300

    program obligatoriu

--- Program.cs
+++ Program.cs
@@ -1,32 +1,54 @@
 ﻿using System;
 
-namespace IntroProject
+public class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+	public static void Main()
+	{
+		Console.WriteLine("Type first number");
+		double x = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Type m,/,s,a");
+		string z = Console.ReadLine();
+		Console.WriteLine("Type second number");
+		double y = Convert.ToDouble(Console.ReadLine());
+		
+		if (z == "s")
+		{
+			Console.WriteLine("Answer: {0}", x + y);
+		}
+		if (z == "a")
+		{
+			Console.WriteLine("Answer: {0}", (x + y)/2 );
+		}
+		if (z == "m")
+		{
+			Console.WriteLine("Answer: {0}", x * y );
+		}
+		if (z == "div")
+		{
+			if (x % y == 0)
+				Console.WriteLine("y divizor pt x");
+		}
+		if (z == "con")
         {
-            int x;
-            Console.Write("X=");
-            string xAsString = Console.ReadLine();
-            x = int.Parse(xAsString);
-
-            int y;            
-            Console.Write("Y=");
-            string yAsString = Console.ReadLine();
-            y = int.Parse(yAsString);
-
-            bool isOverflow = ((x + y) < int.MaxValue);
-            if (!isOverflow)
-            {
-                int sum = checked(x + y);
-                Console.WriteLine("Sum = " + sum);
-            }
-            else
-            {
-                Console.WriteLine("Arithmetic overflow");
-            }
-
+			if (y == x + 1)
+				Console.WriteLine("Nr e cons");
         }
-    }
+		if (z == "max")
+        {
+			if (x > y)
+				Console.WriteLine("x");
+			else
+				Console.WriteLine("y");
+        }
+		
+	}
 }
+/*min e acelasi lucru dar invers
+if (z == "max")
+{
+   if (x > y)
+	   Console.WriteLine("x");
+   else
+	   Console.WriteLine("y");
+}
+*/
